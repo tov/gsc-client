bab9b55990b642d9a44a2336b6fe6c7a1d15cb78
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-16 23:05:44 -0600

    separated out man page updating script.

--- scripts/eecs-install.sh
+++ scripts/eecs-install.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+"$(dirname $0)/update-man-pages.sh"
+
 OPENSSL_STATIC=yes
 export OPENSSL_STATIC
 OPENSSL_INCLUDE_DIR=$TOV_PUB/include
@@ -9,5 +11,3 @@ export OPENSSL_INCLUDE_DIR
 OPENSSL_LIB_DIR=$TOV_PUB/lib64
 export OPENSSL_LIB_DIR
 
-cargo install --verbose --force --path .
-install -m 755 man/gsc.1 $TOV_PUB/share/man/man1/
