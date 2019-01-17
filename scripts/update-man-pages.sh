f1f778060040532a7063435b06874d56ac998b5a
Jesse A. Tov <jesse.tov@gmail.com>
2019-01-16 23:10:29 -0600

    s/-d/-D/

--- scripts/update-man-pages.sh
+++ scripts/update-man-pages.sh
@@ -2,5 +2,5 @@
 
 for i in man/*; do
     section=$(echo "$i" | sed 's/.*[.]//')
-    install -dm 755 "$i" "$TOV_PUB/share/man/man$section/"
+    install -Dm 755 "$i" "$TOV_PUB/share/man/man$section/"
 done
