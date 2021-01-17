0bc9bcb1953294155ed8224208fda46a60b09356
Test Student <cs211@cs.northwestern.edu>
2021-01-17 16:30:49 -0600

    Don't need to specify OpenSSL build environment anymore

--- scripts/eecs-install.sh
+++ scripts/eecs-install.sh
@@ -6,12 +6,12 @@ umask 022
 
 "$(dirname $0)/update-man-pages.sh"
 
-OPENSSL_STATIC=yes
-export OPENSSL_STATIC
-OPENSSL_INCLUDE_DIR=$TOV_PUB/include
-export OPENSSL_INCLUDE_DIR
-OPENSSL_LIB_DIR=$TOV_PUB/lib64
-export OPENSSL_LIB_DIR
+# OPENSSL_STATIC=yes
+# export OPENSSL_STATIC
+# OPENSSL_INCLUDE_DIR=$TOV_PUB/include
+# export OPENSSL_INCLUDE_DIR
+# OPENSSL_LIB_DIR=$TOV_PUB/lib64
+# export OPENSSL_LIB_DIR
 
 if [ "$1" = -l ]; then
     cargo install --verbose --force \
