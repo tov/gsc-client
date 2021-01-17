97715d913ee868a18970ac6ec8217c495d826874
Jesse A. Tov <jesse.tov@gmail.com>
2021-01-17 16:07:42 -0600

    fixup

--- src/lib.rs
+++ src/lib.rs
@@ -88,10 +88,11 @@ impl GscClient {
     }
 
     pub fn admin_divorce(&self, username: &str, hw: usize) -> Result<()> {
-        let creds = self.load_credentials()?;
-        let uri = self.get_uri_for_submission(username, hw, &creds)?;
         let mut message = messages::SubmissionChange::default();
         message.owner2 = Some(());
+
+        let creds = self.load_credentials()?;
+        let uri = self.get_uri_for_submission(username, hw, &creds)?;
         let request = self.http.patch(&uri).json(&message);
         let response = self.send_request(request)?;
         self.print_results(response)
